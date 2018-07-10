892b3904aaa2ba852d6572e528f5ccc27317f3b6
philopian <pdotwdot@gmail.com>
2018-07-10 07:08:06 -0700

    wired up the container with the sockets

--- www/react/components/Container/Container.jsx
+++ www/react/components/Container/Container.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 const uuidv1 = require("uuid/v1");
 import { Wrapper } from "./styles.js";
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:1234");
 
 import DesktopChatButton from "../DesktopChatButton";
 import Header from "../Header";
@@ -9,6 +11,7 @@ import Message from "../Message";
 import NewItem from "../NewItem";
 
 class Container extends Component {
+  username = "";
   state = {
     isOpen: false,
     messages: [
@@ -16,7 +19,8 @@ class Container extends Component {
         key: 0,
         usersMessage: false,
         username: "Some User",
-        message: "ffffff",
+        message:
+          "open up another browser at http://localhost:8080/ to add a message from another user",
         timestamp: "10:10pm"
       }
     ]
@@ -31,22 +35,37 @@ class Container extends Component {
 
   componentDidMount = () => {
     this.scrollToBottom();
+    this.handleSockets();
   };
   componentDidUpdate = () => {
     this.scrollToBottom();
   };
 
+  handleSockets = () => {
+    socket.on("FROM_SERVER", payload => {
+      this.updateCodeFromSockets(payload);
+    });
+  };
+
+  updateCodeFromSockets = payload => {
+    this.setState({
+      messages: this.state.messages.concat(payload)
+    });
+  };
+
   handleNewMessage = message => {
     const d = new Date();
-    this.setState({
-      messages: this.state.messages.concat({
-        key: uuidv1(),
-        usersMessage: true,
-        username: "sdf",
-        message,
-        timestamp: d.toLocaleTimeString()
-      })
-    });
+    // If user isn't defined make a random one
+    if (this.username === "") {
+      this.username = "user_" + Math.floor(Math.random() * 2000) + 1;
+    }
+    const payload = {
+      key: uuidv1(),
+      username: this.username,
+      message,
+      timestamp: d.toLocaleTimeString()
+    };
+    socket.emit("TO_SERVER", payload);
   };
 
   handleFooterClick = () => {
@@ -56,7 +75,14 @@ class Container extends Component {
   };
 
   render() {
-    const allMessages = this.state.messages.map(item => <Message {...item} />);
+    const allMessages = this.state.messages.map(item => {
+      if (item.username === this.username) {
+        item.usersMessage = true;
+      } else {
+        item.usersMessage = false;
+      }
+      return <Message {...item} />;
+    });
 
     return (
       <Wrapper>
